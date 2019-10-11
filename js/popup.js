4b790f873bd3dd1381421ff2edc69e3fb48d72da
Elena Gontareva <e.s.gontareva@gmail.com>
2019-10-11 09:20:07 +0300

    Личный проект: исправляем код для IE

--- js/popup.js
+++ js/popup.js
@@ -27,7 +27,7 @@ var storageName = getItem('name');
 var storageEmail = getItem('email');
 
 var handleClose = function (e) {
-  if (e.path.includes(form)) {
+  if (form.contains(e.target)) {
     return;
   }
 
@@ -66,6 +66,7 @@ close.addEventListener('click', function (e) {
   popup.classList.remove('modal-show');
   popup.classList.remove('modal-error');
   document.body.classList.remove('modal-overlay');
+
 
   window.removeEventListener('click', handleClose);
 });
